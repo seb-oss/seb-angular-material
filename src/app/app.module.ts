7f68b81ef205448673186a7c3b8e1c41b7d909d3
Gabrielė Gelumbauskaitė <gabriele.gelumbauskaite@seb.se>
2019-01-15 13:02:54 +0200

    tabs example added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { MarkdownViewerComponent } from './components/markdown-viewer/markdown-viewer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { TabsDemoComponent } from './components/tabs-demo/tabs-demo.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
     HeaderComponent,
     DemoComponent,
     MarkdownViewerComponent,
+    TabsDemoComponent,
   ],
   imports: [
     BrowserModule,
