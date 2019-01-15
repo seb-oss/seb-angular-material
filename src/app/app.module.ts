17030dc0807b211ded8012b7517b4b765cd762fb
Gabrielė Gelumbauskaitė <gabriele.gelumbauskaite@seb.se>
2019-01-15 12:35:51 +0200

    input/textarea example added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { MarkdownViewerComponent } from './components/markdown-viewer/markdown-viewer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { InputTextareaDemoComponent } from './components/input-textarea-demo/input-textarea-demo.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
     HeaderComponent,
     DemoComponent,
     MarkdownViewerComponent,
+    InputTextareaDemoComponent,
   ],
   imports: [
     BrowserModule,
