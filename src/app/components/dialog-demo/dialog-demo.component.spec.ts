039ac8cf91b0e11852f47c08bf8df721905bcf9a
Zaza Tsitsishvili <zaza.tsitsishvili@seb.se>
2020-12-09 13:16:28 +0200

    Revert "update angular core and cli"
    This reverts commit 0353c431133f6d7b16dd46ff624c1fad2b10e8a5.

--- src/app/components/dialog-demo/dialog-demo.component.spec.ts
+++ src/app/components/dialog-demo/dialog-demo.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DialogDemoComponent } from './dialog-demo.component';
 
@@ -6,7 +6,7 @@ describe('DialogDemoComponent', () => {
   let component: DialogDemoComponent;
   let fixture: ComponentFixture<DialogDemoComponent>;
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ DialogDemoComponent ]
     })
